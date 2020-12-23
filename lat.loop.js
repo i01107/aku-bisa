d9a248e5df079b261f75a890cab710dff5b30b59
LiliAmber <dmraraa@gmail.com>
2020-12-23 18:59:59 +0700

    Rara Danniswara

---
+++ lat.loop.js
@@ -0,0 +1,34 @@
+// 1. menggunakan for, tampilkan angka dari 100 s/d 110
+for(let i = 100; i <= 110; i++) {
+  console.log(i)
+}
+console.log('===')
+
+// 2. menggunakan for, tampilkan angka dari 100 s/d 150, maju per 3 angka
+for(let j = 100; j <= 150; j+=3) {
+  console.log(j)
+}
+console.log('===')
+
+// 3. menggunakan for, tampilkan angka dari 78 s/d 45, mundur per 7 angka
+for(let k = 78; k >= 45; k-=7) {
+  console.log(k)
+}
+console.log('===')
+
+// 4. bikin no 1 versi while
+let x = 100
+
+while(x <= 110) {
+  console.log(x)
+  x++ 
+}
+console.log('===')
+
+// 5. bikin no 3 versi while
+let y = 78
+
+while(y >= 45) {
+  console.log(y)
+  y -=7
+}
