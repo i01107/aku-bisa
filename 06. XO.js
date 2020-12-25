b55ff05c42bd71d21dac17ab4d62cd128716cbf0
LiliAmber <dmraraa@gmail.com>
2020-12-25 17:59:46 +0700

    Rara Danniswara

--- 06. XO.js
+++ 06. XO.js
@@ -1,4 +1,4 @@
-let num = 5
+let num = 7 // input
 
 //output yang diharapkan
 /*
@@ -18,17 +18,17 @@ let batas = (num * 2) - 1
 
 // 2. selesaikan (x sebanyak batas ke kanan):
 // FORMULA
-let temp = ''
-for (let i = 1; i <= batas; i++) {
-  temp += 'x'
-}
-console.log(temp)
+// let temp = ''
+// for (let i = 1; i <= batas; i++) {
+//   temp += 'x'
+// }
+// console.log(temp)
 // ./FORMULA
 
 // 3. selesaikan (x sebanyak num ke bawah):
-for (let j = 1; j <= num; j++) {
-  console.log('x')
-}
+// for (let j = 1; j <= num; j++) {
+//   console.log('x')
+// }
 
 // 4. gabungkan 2 & 3
 /*
@@ -38,6 +38,18 @@ xxxxxxxxx
 xxxxxxxxx
 xxxxxxxxx
 */
+for (let j = 1; j <= num; j++) {
+  // console.log('x')
+  
+  let temp = ''
+  for (let i = 1; i <= batas; i++) {
+    temp += 'x'
+  }
+
+  console.log(temp)
+}
+
+console.log('=====')
 
 // 5. kurangi jumlah x dengan rumus
 /*
@@ -47,6 +59,22 @@ xxxxx
 xxxxxxx
 xxxxxxxxx
 */
+for (let j = 1; j <= num; j++) {
+  // console.log('x')
+  let batas = (j * 2) - 1 
+  // bentuk awal => let batas = (num * 2) - 1
+  // ganti num dengan j, agar dinamis. num tidak bernilai dinamis karena num bukan merupakan variabel looping, 
+  //sehingga nilainya tidak akan berubah jika nilai input num tidak berubah
+
+  let temp = ''
+  for (let i = 1; i <= batas; i++) {
+    temp += 'x'
+  }
+  
+  console.log(temp)
+}
+
+console.log('=====')
 
 // 6. masukin spasi dengan rumus
 /*
@@ -56,6 +84,26 @@ xxxxxxxxx
  xxxxxxx
 xxxxxxxxx
 */
+for (let j = 1; j <= num; j++) {
+  // console.log('x')
+
+  let temp = ''
+  // cetak spasi
+  for (let k = 0; k < (num - j); k++) {
+    // (num - j), digunakan, karena letak spasi sesuai dengan urutan perulangan (4,3,2,1,0)
+  
+    temp += ' '
+  }
+
+  let batas = (j * 2) - 1 
+  for (let i = 1; i <= batas; i++) {
+    temp += 'x'
+  }
+  
+  console.log(temp)
+}
+
+console.log('=====')
 
 // 7. ganti x dengan o di posisi yang seharusnya
 /*
@@ -65,3 +113,26 @@ xxxxxxxxx
  xoxoxox
 xoxoxoxox
 */
+for (let j = 1; j <= num; j++) {
+  // console.log('x')
+  
+  let temp = ''
+  // cetak spasi
+  for (let k = 0; k < (num - j); k++) {
+    temp += ' '
+  }
+
+  let batas = (j * 2) - 1
+  for (let i = 1; i <= batas; i++) {
+    // temp += 'x'
+
+    // ganti x, dengan o jika i genap
+    if(i % 2 === 0) {
+      temp += 'o'
+    } else {
+      temp += 'x'
+    }
+  }
+  
+  console.log(temp)
+}
--- 07. Array.js
+++ 07. Array.js
@@ -89,3 +89,194 @@ for (let j = 0; j < arr2.length; j++) {
 }
 
 console.log(total)
+
+// SOAL 1 :
+let str = '395628'
+
+/*
+333
+999999999
+55555
+666666
+22
+88888888
+*/
+
+// PENYELESAIAN
+/*
+1. selesaikan formula cetak str ke samping
+2. selesaikan cetak str ke bawah
+3. gabungkan 1 dan 2
+4. cetak output sesuai permintaan
+*/
+
+// EKSEKUSI
+// 1. selesaikan formula cetak str ke samping
+let temp = ''
+
+for(let a = 0; a < str.length; a++) {
+  temp += str[a]
+}
+console.log(temp)
+console.log('=====')
+
+// 2. selesaikan cetak str ke bawah
+for(let b = 0; b < str.length; b++) {
+  console.log(str[b])
+}
+console.log('=====')
+
+// 3. gabungkan 1 dan 2
+for(let b = 0; b < str.length; b++) {
+  // console.log(str[b])
+
+  let temp = ''
+
+  for(let a = 0; a < str.length; a++) {
+    temp += str[a]
+  }
+
+  console.log(temp)
+}
+console.log('=====')
+
+// 4. cetak output sesuai permintaan
+for(let b = 0; b < str.length; b++) {
+  // console.log(str[b])
+
+  let temp = ''
+
+  for(let a = 0; a <= b; a++) {
+    temp += str[a]
+  }
+
+  console.log(temp)
+}
+console.log('=====')
+
+// SOAL 2 :
+let arr = [
+  [1, 2, 3],
+  [4, 5],
+  [9, 8, 4],
+  [13, 8, 10, 100, 55]
+]
+
+/*
+Mengeluarkan 3 output :
+- jumlah total
+- jumlah total dari bilangan yang GENAP saja
+- jumlah total dari bilangan yang GANJIL saja
+- jumlah total dari bilangan yang GENAP DAN di bawah 10 saja
+*/
+
+// PENYELESAIAN
+/*
+1. buat array dengan 1 element dari soal, biasanya index 0
+2. buat iterasi array utama
+3. gabungkan FORMULA ke dalam step 2, mengganti console.log
+4. bikin penyesuaian ( varible, dll)
+5. sisipkan langkah untuk menjumlahkan (sesuai output) :
+
+- siapkan variable penampung:
+  - hasil penjumlahan seluruhnya
+  - hasil penjumlahan bilangan GENAP 
+  - hasil penjumlahan bilangan GANJIL
+  - hasil penjumlahan bilangan GENAP di bawah 10
+- langsung tambahin console.log di akhir, beri jeda beberapa baris kosong
+- copy paste hasil no 4, SESUAIKAN dengan cara penjumlahan yang di dalem nested :
+  - penjumlahan seluruhnya
+  - penjumlahan bilangan GENAP :
+    - cek dengan kondisi, jika indeks % 2 = 0, maka jumlahkan, selain itu, jumlahkan ke var jumalah bil.GANJIL
+  - penjumlahan bilangan GANJIL
+  - penjumlahan bilangan GENAP di bawah 10 :
+    - cek dengan kondisi, jika indeks genap && indeks < 10
+- ganti console.log nya dengan variable penampung
+
+*/
+
+// EKSEKUSI
+
+// 1. buat array dengan 1 element dari soal, biasanya index 0
+let temp = [
+  [1, 2, 3]
+]
+console.log('=====')
+
+// FORMULA
+for (let i = 0; i < temp[0].length; i++) {
+  console.log(temp[0][i])
+}
+// ./FORMULA
+
+// 2. buat iterasi array utama
+for (let j = 0; j < arr.length; j++) {
+  console.log(arr[j])
+}
+console.log('=====')
+
+// 3. gabungkan FORMULA ke dalam step 2, mengganti console.log
+for (let j = 0; j < arr.length; j++) {
+  // FORMULA
+  for (let i = 0; i < temp[0].length; i++) {
+    console.log(temp[0][i])
+  }
+  // ./FORMULA
+}
+console.log('=====')
+
+// 4. bikin penyesuaian ( varible, dll)
+for (let j = 0; j < arr.length; j++) {
+  // FORMULA
+  for (let i = 0; i < arr[j].length; i++) {
+    console.log(arr[j][i])
+  }
+  // ./FORMULA
+}
+console.log('=====')
+
+// 5. sisipkan langkah untuk menjumlahkan (sesuai output) :
+/*
+- siapkan variable penampung:
+- hasil penjumlahan seluruhnya
+- hasil penjumlahan bilangan GENAP 
+- hasil penjumlahan bilangan GANJIL
+- hasil penjumlahan bilangan GENAP di bawah 10
+- langsung tambahin console.log di akhir, beri jeda beberapa baris kosong
+- copy paste hasil no 4, SESUAIKAN dengan cara penjumlahan yang di dalem nested :
+- penjumlahan seluruhnya
+- penjumlahan bilangan GENAP :
+  - cek dengan kondisi, jika indeks % 2 = 0, maka jumlahkan, selain itu, jumlahkan ke var jumalah bil.GANJIL
+- penjumlahan bilangan GANJIL
+- penjumlahan bilangan GENAP di bawah 10 :
+  - cek dengan kondisi, jika indeks genap && indeks < 10
+- ganti console.log nya dengan variable penampung
+*/
+
+let total = 0
+let totalGenap = 0
+let totalGanjil = 0
+let totalGenap1 = 0
+
+for (let j = 0; j < arr.length; j++) {
+  // FORMULA
+  for (let i = 0; i < arr[j].length; i++) {
+    total += arr[j][i]
+
+    if(arr[j][i] % 2 === 0) {
+      totalGenap += arr[j][i]
+    } else {
+      totalGanjil += arr[j][i]
+    }
+
+    if(arr[j][i] % 2 === 0 && arr[j][i] < 10) {
+      totalGenap1 += arr[j][i]
+    }
+  }
+  // ./FORMULA
+}
+
+console.log(total)
+console.log(totalGenap)
+console.log(totalGanjil)
+console.log(totalGenap1)
